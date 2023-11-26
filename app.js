661f73ea0fc05be0fb1d0e43ee0d65c3160e6575
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-26 15:40:10 +0700

    Filter by text finished

--- app.js
+++ app.js
@@ -17,3 +17,15 @@ const displayProducts = () => {
     .join("");
 };
 displayProducts();
+
+// text filter
+const form = document.querySelector(".input-form");
+const searchInput = document.querySelector(".search-input");
+
+form.addEventListener("keyup", () => {
+  const inputValue = searchInput.value;
+  filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue);
+  });
+  displayProducts();
+});
