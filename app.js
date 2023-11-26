f746c3518453b702d8bb16aa8bcda9099d595fcf
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-26 12:24:27 +0700

    Import local data and render it

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('filters project');
+let filteredProducts = [...products];
+const productsContainer = document.querySelector(".products-container");
+
+const displayProducts = () => {
+  // if statement
+  productsContainer.innerHTML = filteredProducts
+    .map(({ id, title, image, price }) => {
+      return ` <!-- single product -->
+          <article class="product" data-id=${id}>
+            <img src=${image} class="product-img img">
+            <footer>
+              <h5 class="product-name">${title}</h5>
+              <span class="product-price">$${price}</span>
+            </footer>
+          </article>`;
+    })
+    .join("");
+};
+displayProducts();
